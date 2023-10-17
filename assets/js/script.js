4d5c412d111e43577b2cff1fe99aa061333e5a21
TonichaB <>
2023-10-17 18:03:57 +0000

    Feat: add variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,15 @@
 const playGame = document.getElementById("play-btn");
 const instructionsButton = document.getElementById("how-to-play-btn");
 const question = document.getElementById("question");
+const buttonA = document.getElementById("answer_a_btn")
+const buttonB = document.getElementById("answer_b_btn");
+const buttonC = document.getElementById("answer_c_btn");
+const buttonD = document.getElementById("answer_d_btn");
 
 let score = 0;
+let wrongAnswer;
+let correctAnswer;
+let shuffleQuestion;
 
 
 /* Functions */
@@ -13,6 +20,7 @@ function checkAnswer();
 function incrementScore();
 function displayQuestion();
 function openPopUp();
+function closePopUp();
 function nextQuestion();
 function updateQNA();
 function shuffleArray();
