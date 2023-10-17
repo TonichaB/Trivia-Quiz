b2fe0c6423ccf73cf3c5b703bbf0ddb87662d21a
TonichaB <>
2023-10-17 18:59:08 +0000

    Feat: add first event listeners following DOM loading to listen for start page buttons to be clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 const playGame = document.getElementById("play-btn");
 const instructionsButton = document.getElementById("how-to-play-btn");
 const question = document.getElementById("question");
-const buttonA = document.getElementById("answer_a_btn")
+const buttonA = document.getElementById("answer_a_btn");
 const buttonB = document.getElementById("answer_b_btn");
 const buttonC = document.getElementById("answer_c_btn");
 const buttonD = document.getElementById("answer_d_btn");
@@ -13,17 +13,33 @@ let wrongAnswer;
 let correctAnswer;
 let shuffleQuestion;
 
+/* Wait for DOM to load before executing the first function 
+* to show the start page and add event listeners for the buttons
+*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const startButtons = document.getElementsByClassName("btn");
+    startButtons.forEach(startButton) => {
+        startButtons.addEventListener("click", function () {
+            if (this.getAtribute("id") === ".play-btn") {
+                runGame();
+            } else if (this.getAtribute("id") === "how-to-play-btn") {
+                instructionsPopUp();
+            }
+        });
+    };
+});
 
 /* Functions */
 function runGame();
 function checkAnswer();
 function incrementScore();
 function displayQuestion();
-function openPopUp();
+function instructionsPopUp();
 function closePopUp();
 function nextQuestion();
 function updateQNA();
 function shuffleArray();
 function retrieveContent() {
     fetch('https://the-trivia-api.com/v2/questions');
-};
+}
